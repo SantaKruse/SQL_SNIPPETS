053726def8d9dce52bdbe55df6dee314741d5041
Dan Kruse <dkruse22@live.co.uk>
2018-01-26 18:33:41 +0000

    Create Fetch CKD Stage.sql

---
+++ Fetch CKD Stage.sql
@@ -0,0 +1,39 @@
+USE [Diabetes_i3_Outputs]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE	FUNCTION	[dbo].[UF_CKD_STAGE] 
+(
+			@IN	AS	FLOAT
+
+)
+RETURNS		VARCHAR(2)
+AS
+BEGIN
+			DECLARE		@OUT	AS	VARCHAR(2)
+			
+			SET @OUT = (CASE 
+						WHEN @IN IS NULL THEN NULL
+						WHEN @IN >= 200 THEN 'OR'
+						WHEN @IN >= 90 THEN '1'
+						WHEN @IN >= 60 THEN '2'
+						WHEN @IN >= 45 THEN '3a'
+						WHEN @IN >= 30 THEN '3b'
+						WHEN @IN >= 15 THEN '4'
+						WHEN @IN >= 0.01 THEN '5'
+						WHEN @IN < 0.01 THEN 'OR'
+						END)
+											
+			RETURN	@OUT
+
+END
+
+
+GO
