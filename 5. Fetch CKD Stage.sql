a5b6163910f07d28a63640ed7225fd22d6fd9e34
Dan Kruse <dkruse22@live.co.uk>
2018-01-26 18:37:31 +0000

    Update 5. Fetch CKD Stage.sql

--- 5. Fetch CKD Stage.sql
+++ 5. Fetch CKD Stage.sql
@@ -1,28 +1,25 @@
-CREATE	FUNCTIOn UFF_CKD_STAGE 
-(
-			@IN	AS	FLOAT
+/******************************
+** NAME: FETCH_CKD_STAGE.SQL   
+** DESC: FUNCTION TO RETURN CKD STAGE
+** AUTH: HTTPS://GITHUB.COM/SANTAKRUSE
+** DATE: 26/01/2018
+**************************/
 
-)
-RETURNS		VARCHAR(2)
+CREATE	FUNCTION FETCH_CKD_STAGE(@IN AS FLOAT) 
+RETURNS VARCHAR(2)
 AS
 BEGIN
-			DECLARE		@OUT	AS	VARCHAR(2)
-			
-			SET @OUT = (CASE 
-						WHEN @IN IS NULL THEN NULL
-						WHEN @IN >= 200 THEN 'OR'
-						WHEN @IN >= 90 THEN '1'
-						WHEN @IN >= 60 THEN '2'
-						WHEN @IN >= 45 THEN '3a'
-						WHEN @IN >= 30 THEN '3b'
-						WHEN @IN >= 15 THEN '4'
-						WHEN @IN >= 0.01 THEN '5'
-						WHEN @IN < 0.01 THEN 'OR'
-						END)
-											
-			RETURN	@OUT
-
+	DECLARE	@OUT AS	VARCHAR(2)
+	SET @OUT = (CASE 
+			WHEN @IN IS NULL THEN NULL
+			WHEN @IN >= 200 THEN 'OR'
+			WHEN @IN >= 90 THEN '1'
+			WHEN @IN >= 60 THEN '2'
+			WHEN @IN >= 45 THEN '3a'
+			WHEN @IN >= 30 THEN '3b'
+			WHEN @IN >= 15 THEN '4'
+			WHEN @IN >= 0.01 THEN '5'
+			WHEN @IN < 0.01 THEN 'OR'
+		    END)									
+	RETURN	@OUT
 END
-
-
-GO
